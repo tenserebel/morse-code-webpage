8b2652ebc3e93693fcc36fe84607b2778ebea8ad
Prem Jha <63343297+tenserebel@users.noreply.github.com>
2021-05-27 21:32:22 +0530

    Add files via upload

--- morse code/app.py
+++ morse code/app.py
@@ -17,15 +17,8 @@ def txt_to_morse(input):
     cipher = ''
     for letter in message:
         if letter != ' ':
-  
-            # Looks up the dictionary and adds the
-            # correspponding morse code
-            # along with a space to separate
-            # morse codes for different characters
             cipher += MORSE_CODE_DICT[letter] + ' '
         else:
-            # 1 space indicates different characters
-            # and 2 indicates different words
             cipher += ' '
             
   
@@ -40,28 +33,17 @@ def morse_to_txt(input):
     citext = ''
     for letter in message:
   
-        # checks for space
         if (letter != ' '):
   
-            # counter to keep track of space
             i = 0
-  
-            # storing morse code of a single character
             citext += letter
   
-        # in case of space
         else:
-            # if i = 1 that indicates a new character
             i += 1
   
-            # if i = 2 that indicates a new word
             if i == 2 :
-  
-                 # adding space to separate words
                 decipher += ' '
             else:
-  
-                # accessing the keys using their values (reverse of encryption)
                 decipher += list(MORSE_CODE_DICT.keys())[list(MORSE_CODE_DICT
                 .values()).index(citext)]
                 citext = ''
